53accdfe8af408de59cfc144de1bd3b619972edd
Nikita Chernega <60040399+Nikitachr@users.noreply.github.com>
2020-11-04 15:28:34 +0200

    fix

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const compression = require('compression')
 
 const app = express();
 
+app.use(compression());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/WeatherApp'));
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/WeatherApp/index.html'));
 });
 
+const port = process.env.PORT || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+server.listen(port, () => console.log('server started'));
